1f0091430f8c5b31209e136d18104cb58db0f838
李毅 <thomas@23-6.site>
2021-01-09 03:18:38 +0900

    追加模板

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # トウキ合戦
 
-Quizizz、Kahoot!などのゲームベースの学習プラットフォーム向けの問題ファイル（Excel）を生成する小道具です。
+Quizizz/Kahoot! などのゲームベースの学習プラットフォーム向けの問題ファイル（Excel）を生成する小道具です。
 
 ## 技術情報
 
@@ -8,21 +8,29 @@ Quizizz、Kahoot!などのゲームベースの学習プラットフォーム向
 
 2. ライブラリー：
 
-   `エクセル処理` **xlrd** **xlwt**
+   `エクセル処理` **openpyxl**
 
    ```shell
-   pip install xlrd xlwt
+   pip install openpyxl
    ```
 
    `データベース` **sqlite3**
 
 ## 利用方法
 
-（つづく）
+1. **input/** フォルダの下に、"**quizizz.xlsx**" または "**kahoot.xlsx**" の入力ファイルを入れておいてください。
+
+2. システムが Windows の場合は、**Run-Win.bat** をダブルクリックしてください。
+
+   システムが macOS の場合は、**Run-Mac.command** をダブルクリックしてください。
+
+3. 実行が完了したら、**output/** フォルダの下に、入力ファイルと同じ名前のファイルが生成されました。
+
+   このファイルを Quizizz/Kahoot! にアップロードして、ゲームを楽しみましょう！
 
 ## 参考記事
 
-1. [python读写Excel表格（简单实用）](https://blog.csdn.net/CjhLoveAndroid/article/details/103566321)| 一giao窝里giao
+1. [openpyxl による Excelファイル操作方法のまとめ](https://gammasoft.jp/support/how-to-use-openpyxl-for-excel-file/) | GAMMASOFT
 2. [python3でsqlite3の操作。作成や読み出しなどの基礎。](https://qiita.com/saira/items/e08c8849cea6c3b5eb0c)| saira
 3. [How can I add the sqlite3 module to Python?](https://stackoverflow.com/questions/19530974/how-can-i-add-the-sqlite3-module-to-python) | falsetru
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+from openpyxl import load_workbook
 
 
 def test():
@@ -7,15 +8,92 @@ def test():
 
 
 def create_database():
+    # データベースをクリアする
     os.remove('sqlite/database.db')
+    # データベースを作成する
     db = sqlite3.connect('sqlite/database.db')
     handler = db.cursor()
+    # テーブルを作成する
     handler.execute(
         'CREATE TABLE vocabulary(id INTEGER PRIMARY KEY AUTOINCREMENT, word STRING, meaning STRING)')
+    # 変更を反映する
     db.commit()
     db.close()
 
 
+def import_data():
+    # ファイルを読み込む
+    if os.path.exists('input/kahoot.xlsx'):
+        # Kahoot!
+        wb = load_workbook('input/kahoot.xlsx')
+    elif os.path.exists('input/quizizz.xlsx'):
+        # Quizizz
+        wb = load_workbook('input/quizizz.xlsx')
+    else:
+        # File Not Found
+        print('Oops!  File "kahoot.xlsx" or "quizizz.xlsx" is not found in "input/".')
+        exit(1)
+    # データを取り込む
+    sheet1 = wb.worksheets[0]
+    print(sheet1['A1'].value)
+
+
+def generate_upload_file():
+    if os.path.exists('input/kahoot.xlsx'):
+        # Kahoot!
+        os.remove('output/kahoot.xlsx')
+        generate_kahoot()
+        os.remove('input/kahoot.xlsx')
+    elif os.path.exists('input/quizizz.xlsx'):
+        # Quizizz
+        os.remove('output/quizizz.xlsx')
+        generate_quizizz()
+        os.remove('input/quizizz.xlsx')
+    else:
+        # 何もしない
+        pass
+
+
+def generate_kahoot():
+    # Kahoot!
+    wb = load_workbook('template/kahoot.xlsx')
+    sheet1 = wb.worksheets[0]
+    curRow = 9
+    # フォーマット定義
+    colQuestion = 'B'
+    colOption1 = 'C'
+    colOption2 = 'D'
+    colOption3 = 'E'
+    colOption4 = 'F'
+    colTimeLimit = 'G'
+    colAnswer = 'H'
+    # TODO:
+    wb.save('output/kahoot.xlsx')
+    pass
+
+
+def generate_quizizz():
+    # Quizizz
+    wb = load_workbook('template/quizizz.xlsx')
+    sheet1 = wb.worksheets[0]
+    curRow = 3
+    # フォーマット定義
+    colQuestion = 'A'
+    colType = 'B'
+    colOption1 = 'C'
+    colOption2 = 'D'
+    colOption3 = 'E'
+    colOption4 = 'F'
+    colOption5 = 'G'
+    colAnswer = 'H'
+    colTimeLimit = 'I'
+    colImageLink = 'J'
+    # TODO:
+    wb.save('output/quizizz.xlsx')
+    pass
+
+
 if __name__ == '__main__':
     create_database()
-    test()
+    import_data()
+    generate_upload_file()
