5ac84cb54dd9d1872e77f0e4da66f73b75956937
李毅 <thomas@23-6.site>
2021-01-15 00:51:45 +0900

    增强功能

--- main.py
+++ main.py
@@ -4,43 +4,45 @@ from openpyxl import load_workbook
 from random import randrange
 
 
-def import_data(handler):
+def import_data(handler, platform):
     # ファイルを読み込む
-    if os.path.exists('input/kahoot.xlsx'):
+    if platform == "kahoot":
         # Kahoot!
-        wb = load_workbook('input/kahoot.xlsx')
-    elif os.path.exists('input/quizizz.xlsx'):
+        wb = load_workbook("input/kahoot.xlsx")
+    elif platform == "quizizz":
         # Quizizz
-        wb = load_workbook('input/quizizz.xlsx')
+        wb = load_workbook("input/quizizz.xlsx")
     else:
-        # File Not Found
-        print('Oops! File "kahoot.xlsx" or "quizizz.xlsx" is not found in "input/".')
-        exit(1)
+        # 何もしない
+        pass
     # データを取り込む
     vocabulary = []
     ws = wb.worksheets[0]
     for row in ws.rows:
         if row[0].value is None:
+            # Empty Word
             print('Oops! "' + row[0].coordinate + '" is EMPTY.')
             exit(1)
         if row[1].value is None:
+            # Empty Meaning
             print('Oops! "' + row[1].coordinate + '" is EMPTY.')
             exit(1)
         vocabulary.append((row[0].value, row[1].value))
     # データベースにインポートする
     handler.executemany(
-        "INSERT INTO vocabulary(word, meaning) VALUES (?, ?)",  vocabulary)
+        "INSERT INTO vocabulary(word, meaning) VALUES (?, ?)", vocabulary
+    )
 
 
-def generate_upload_file(handler):
-    if os.path.exists('input/kahoot.xlsx'):
+def generate_upload_file(handler, platform):
+    if platform == "kahoot":
         # Kahoot!
         generate_kahoot(handler)
-        os.remove('input/kahoot.xlsx')
-    elif os.path.exists('input/quizizz.xlsx'):
+        os.rename("input/kahoot.xlsx", "input/_kahoot.xlsx")
+    elif platform == "quizizz":
         # Quizizz
         generate_quizizz(handler)
-        os.remove('input/quizizz.xlsx')
+        os.rename("input/quizizz.xlsx", "input/_quizizz.xlsx")
     else:
         # 何もしない
         pass
@@ -48,20 +50,21 @@ def generate_upload_file(handler):
 
 def generate_kahoot(handler):
     # Kahoot!
-    wb = load_workbook('template/kahoot.xlsx')
+    wb = load_workbook("template/kahoot.xlsx")
     ws = wb.worksheets[0]
     curRow = 9
     # フォーマット定義
-    colQuestion = 'B'
-    colOption1 = 'C'
-    colOption2 = 'D'
-    colOption3 = 'E'
-    colOption4 = 'F'
-    colTimeLimit = 'G'
-    colAnswer = 'H'
+    colQuestion = "B"
+    colOption1 = "C"
+    colOption2 = "D"
+    colOption3 = "E"
+    colOption4 = "F"
+    colTimeLimit = "G"
+    colAnswer = "H"
     # データを抽出する
     questions = handler.execute(
-        'SELECT * FROM vocabulary ORDER BY RANDOM() LIMIT 20').fetchall()
+        "SELECT * FROM vocabulary ORDER BY RANDOM() LIMIT 20"
+    ).fetchall()
     for question in questions:
         questionId = question[0]
         questionWord = question[1]
@@ -70,7 +73,10 @@ def generate_kahoot(handler):
         ws[colQuestion + str(curRow)] = questionWord
         # 選択肢を作成する
         options = handler.execute(
-            'SELECT meaning FROM vocabulary WHERE id <> ' + str(questionId) + ' ORDER BY RANDOM() LIMIT 4').fetchall()
+            "SELECT meaning FROM vocabulary WHERE id <> "
+            + str(questionId)
+            + " ORDER BY RANDOM() LIMIT 4"
+        ).fetchall()
         ws[colOption1 + str(curRow)] = options[0][0]
         ws[colOption2 + str(curRow)] = options[1][0]
         ws[colOption3 + str(curRow)] = options[2][0]
@@ -93,28 +99,29 @@ def generate_kahoot(handler):
             pass
         curRow += 1
     # ファイルを保存する
-    wb.save('output/kahoot.xlsx')
+    wb.save("output/kahoot.xlsx")
 
 
 def generate_quizizz(handler):
     # Quizizz
-    wb = load_workbook('template/quizizz.xlsx')
+    wb = load_workbook("template/quizizz.xlsx")
     ws = wb.worksheets[0]
     curRow = 3
     # フォーマット定義
-    colQuestion = 'A'
-    colQuestionType = 'B'
-    colOption1 = 'C'
-    colOption2 = 'D'
-    colOption3 = 'E'
-    colOption4 = 'F'
-    colOption5 = 'G'
-    colAnswer = 'H'
-    colTimeLimit = 'I'
-    colImageLink = 'J'
+    colQuestion = "A"
+    colQuestionType = "B"
+    colOption1 = "C"
+    colOption2 = "D"
+    colOption3 = "E"
+    colOption4 = "F"
+    colOption5 = "G"
+    colAnswer = "H"
+    colTimeLimit = "I"
+    colImageLink = "J"
     # データを抽出する
     questions = handler.execute(
-        'SELECT * FROM vocabulary ORDER BY RANDOM() LIMIT 20').fetchall()
+        "SELECT * FROM vocabulary ORDER BY RANDOM() LIMIT 20"
+    ).fetchall()
     for question in questions:
         questionId = question[0]
         questionWord = question[1]
@@ -122,10 +129,13 @@ def generate_quizizz(handler):
         # 問題文を作成する
         ws[colQuestion + str(curRow)] = questionWord
         # 問題のタイプを指定する
-        ws[colQuestionType + str(curRow)] = 'Multiple Choice'
+        ws[colQuestionType + str(curRow)] = "Multiple Choice"
         # 選択肢を作成する
         options = handler.execute(
-            'SELECT meaning FROM vocabulary WHERE id <> ' + str(questionId) + ' ORDER BY RANDOM() LIMIT 5').fetchall()
+            "SELECT meaning FROM vocabulary WHERE id <> "
+            + str(questionId)
+            + " ORDER BY RANDOM() LIMIT 5"
+        ).fetchall()
         ws[colOption1 + str(curRow)] = options[0][0]
         ws[colOption2 + str(curRow)] = options[1][0]
         ws[colOption3 + str(curRow)] = options[2][0]
@@ -150,28 +160,60 @@ def generate_quizizz(handler):
             # 何もしない
             pass
         # 画像リンクを空にする
-        ws[colImageLink + str(curRow)] = ''
+        ws[colImageLink + str(curRow)] = ""
         curRow += 1
     # ファイルを保存する
-    wb.save('output/quizizz.xlsx')
+    wb.save("output/quizizz.xlsx")
 
 
-if __name__ == '__main__':
+def main(platform):
     # データベースを作成する
-    if os.path.exists('sqlite/database.db'):
-        os.remove('sqlite/database.db')
-    db = sqlite3.connect('sqlite/database.db')
+    if os.path.exists("sqlite/database.db"):
+        os.remove("sqlite/database.db")
+    db = sqlite3.connect("sqlite/database.db")
     handler = db.cursor()
     # テーブルを作成する
     handler.execute(
-        'CREATE TABLE vocabulary(id INTEGER PRIMARY KEY AUTOINCREMENT, word STRING, meaning STRING)')
+        "CREATE TABLE vocabulary(id INTEGER PRIMARY KEY AUTOINCREMENT, word STRING, meaning STRING)"
+    )
     # データをインポートする
-    import_data(handler)
+    import_data(handler, platform)
     # 変更を反映する
     db.commit()
     # アップロード用ファイルを生成する
-    generate_upload_file(handler)
+    generate_upload_file(handler, platform)
     # データベースの接続を解除する
     db.close()
     # データベースを削除する
-    os.remove('sqlite/database.db')
+    os.remove("sqlite/database.db")
+
+
+if __name__ == "__main__":
+
+    if os.path.exists("input/_kahoot.xlsx"):
+        # Kahoot!
+        if os.path.exists("input/kahoot.xlsx"):
+            os.remove("input/_kahoot.xlsx")
+        else:
+            os.rename("input/_kahoot.xlsx", "input/kahoot.xlsx")
+
+    if os.path.exists("input/_quizizz.xlsx"):
+        # Quizizz
+        if os.path.exists("input/quizizz.xlsx"):
+            os.remove("input/_quizizz.xlsx")
+        else:
+            os.rename("input/_quizizz.xlsx", "input/quizizz.xlsx")
+
+    emptyInput = True
+    if os.path.exists("input/kahoot.xlsx"):
+        # Kahoot!
+        emptyInput = False
+        main("kahoot")
+    if os.path.exists("input/quizizz.xlsx"):
+        # Quizizz
+        emptyInput = False
+        main("quizizz")
+    if emptyInput is True:
+        # File Not Found
+        print('Oops! File "kahoot.xlsx" or "quizizz.xlsx" is not found in "input/".')
+        exit(1)
